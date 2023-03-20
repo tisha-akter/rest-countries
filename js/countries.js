ab26a2a5ff198c7ae4dd794952f5ec2741678fab
tisha-akter <tishaakter.jui79@gmail.com>
2023-03-20 15:59:29 +0600

    added style.css and js file

---
+++ js/countries.js
@@ -0,0 +1,26 @@
+const loadCountries = () =>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res. json())
+    .then(data =>displayCountries(data));
+}
+
+const displayCountries = countries =>{
+    console.log(countries[0]);
+    const CountriesHTML = countries.map(country => getCountryHTML(country));
+    // console.log(CountriesHTML[0]);
+    const container = document.getElementById('countries');
+    container.innerHTML = CountriesHTML.join(' ');
+
+}
+
+const getCountryHTML = country => {
+    return `
+        <div class = "country">
+            <h2>${country.name.common} </h2>
+            <img src="${country.flags.png}">
+        </div>
+
+    `
+}
+
+loadCountries();
